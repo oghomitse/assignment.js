ea8d85ef532f63eaea4cfbe32bbc8db2707ca5df
oghomitse aghogho <saintflexi1@yahoo.com>
2021-06-29 11:08:39 +0100

    First commit, assignment for week4 done

---
+++ index.js
@@ -0,0 +1,24 @@
+// let admin;
+// let quest;
+// quest = "john;
+// admin = quest;
+// console.log(admin);
+
+// for (for let i = 1; i<101; i++){
+//     console. log(i);
+// }
+// min = (a,b) =>{
+//     if (a-b < 0){
+//         return b
+//         else if (b = a){
+//             return " a and b are equal"
+//         }
+//     }
+
+// for (let i = 1; i<101; i++){
+//     if(i%3==0&&i%5!=0){
+//         console.log("buzz");
+//     }
+//     else {console.log (i)};
+// }
+// }
